ea6115de6b639f0972ac9fb8dbe0d74486a3469c
Johnny952 <jonathan.espinola@ug.uchile.cl>
2021-11-29 18:31:29 -0300

    delete rm and opengl from run carla script

--- scripts/run_carla.sh
+++ scripts/run_carla.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name carla-server -d --rm -p 2000-2002:2000-2002 --runtime=nvidia -it carlasim/carla:0.9.11 /bin/bash -c 'SDL_VIDEODRIVER=offscreen ./CarlaUE4.sh -opengl -quality-level=low -world-port=2000 -benchmark -carla-server -carla-no-hud'
+docker run --name carla-server -d -p 2000-2002:2000-2002 --runtime=nvidia -it carlasim/carla:0.9.11 /bin/bash -c 'SDL_VIDEODRIVER=offscreen ./CarlaUE4.sh -quality-level=low -world-port=2000 -benchmark -carla-server -carla-no-hud'
