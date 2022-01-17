d3bf5f9059ebda278d466979bf34e28577924997
Johnny952 <jonathan.espinola@ug.uchile.cl>
2022-01-17 13:22:22 -0300

    Add image segmentation banch as decoder

--- src/vae/vae_backbone.py
+++ src/vae/vae_backbone.py
@@ -25,6 +25,8 @@ __TIM_MODELS__ = {
     'efficientnet_lite0': 320,
     'efficientnet_lite4': 448,
 }
+
+
 class TimmBackbone(nn.Module):
     def __init__(self, model_name: str = "mobilenetv2_100", in_channels=4, out_channels=512):
         super(TimmBackbone, self).__init__()
@@ -48,24 +50,6 @@ class TimmBackbone(nn.Module):
         x = self.pool_adjust_dim(x)
         return x
 
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=(3, 3), padding=(1, 1), bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=(3, 3), padding=(1, 1), bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
 
 class BasicDecoder(nn.Module):
     def __init__(self, latent_dim, num_clases):
@@ -111,13 +95,38 @@ class BasicDecoder(nn.Module):
         x = self.final_layer(x)
         return x
 
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=(
+                3, 3), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=(
+                3, 3), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
 class UpConvBlock(nn.Module):
     """
     x2 space resolution
     """
+
     def __init__(self, in_channels: int, out_channels: int, **kwargs):
         super(UpConvBlock, self).__init__()
-        self.up_conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=(3, 3), stride=(2, 2), **kwargs)
+        self.up_conv = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size=(3, 3), stride=(2, 2), **kwargs)
         self.double_conv = DoubleConv(out_channels, out_channels)
 
     def forward(self, x):
@@ -125,21 +134,33 @@ class UpConvBlock(nn.Module):
         x = self.double_conv(x)
         return x
 
+
 class ImageSegmentationBranch(nn.Module):
 
-    def __init__(self, in_channels: int, output_channels: int):
+    def __init__(self, in_channels: int, output_channels: int, latent_dim=512):
         super(ImageSegmentationBranch, self).__init__()
+        self.decoder_input = nn.Linear(latent_dim, in_channels * (3 * 3))
+
         self.in_channels = in_channels
         self.up1 = UpConvBlock(in_channels, in_channels // 2)
-        self.up2 = UpConvBlock(in_channels // 2, in_channels // 4, padding=(1, 1), output_padding=(1, 1))
-        self.up3 = UpConvBlock(in_channels // 4, in_channels // 8, padding=(1, 1), output_padding=(1, 1))
-        self.up4 = UpConvBlock(in_channels // 8, in_channels // 16, padding=(1, 1), output_padding=(1, 1))
-        self.up5 = UpConvBlock(in_channels // 16, in_channels // 32, padding=(1, 1), output_padding=(1, 1))
-        self.up6 = UpConvBlock(in_channels // 32, in_channels // 64, padding=(1, 1), output_padding=(1, 1))
+        self.up2 = UpConvBlock(
+            in_channels // 2, in_channels // 4, padding=(1, 1), output_padding=(1, 1))
+        self.up3 = UpConvBlock(
+            in_channels // 4, in_channels // 8, padding=(1, 1), output_padding=(1, 1))
+        self.up4 = UpConvBlock(
+            in_channels // 8, in_channels // 16, padding=(1, 1), output_padding=(1, 1))
+        self.up5 = UpConvBlock(
+            in_channels // 16, in_channels // 32, padding=(1, 1), output_padding=(1, 1))
+        self.up6 = UpConvBlock(
+            in_channels // 32, in_channels // 64, padding=(1, 1), output_padding=(1, 1))
         # self.output_conv = nn.Conv2d(in_channels // 64, output_channels, kernel_size=(1, 1))
-        self.output_conv = OutputConv(in_channels // 64, output_channels, mid_channels=in_channels // 128)
+        self.output_conv = OutputConv(
+            in_channels // 64, output_channels, mid_channels=in_channels // 128)
 
     def forward(self, x):
+        x = self.decoder_input(x)
+        x = x.view(-1, self.in_channels, 3, 3)
+
         x = self.up1(x)
         x = self.up2(x)
         x = self.up3(x)
@@ -149,13 +170,15 @@ class ImageSegmentationBranch(nn.Module):
         x = self.output_conv(x)
         return x
 
+
 class OutputConv(nn.Module):
 
     def __init__(self, in_channels: int, out_channels: int, mid_channels: int):
         super(OutputConv, self).__init__()
         self.double_conv = DoubleConv(in_channels, out_channels, mid_channels)
         self.avg_pool = nn.AdaptiveAvgPool2d((288, 288))
-        self.last_layer = nn.Conv2d(out_channels, out_channels, kernel_size=(1, 1))
+        self.last_layer = nn.Conv2d(
+            out_channels, out_channels, kernel_size=(1, 1))
 
     def forward(self, x):
         x = self.double_conv(x)
@@ -170,8 +193,8 @@ class VAEBackbone(BaseVAE):
                  num_clases: int = 6,
                  in_channels: int = 4,
                  loss=None,
-                 backbone: str='efficientnet_l2',
-                 out_backbone: int=512,
+                 backbone: str = 'efficientnet_l2',
+                 out_backbone: int = 512,
                  **kwargs) -> None:
         super(VAEBackbone, self).__init__()
         self.out_backbone = out_backbone
@@ -183,13 +206,12 @@ class VAEBackbone(BaseVAE):
             self.loss_fn = nn.CrossEntropyLoss()
 
         self.encoder = TimmBackbone(backbone, in_channels, out_backbone)
-        
+
         self.fc_mu = nn.Linear(self.pred_dims, latent_dim)
         self.fc_var = nn.Linear(self.pred_dims, latent_dim)
 
-        self.pre_decoder = nn.Linear(latent_dim, 256*3*3)
-        self.decoder = BasicDecoder(latent_dim, num_clases)
-        # self.decoder = ImageSegmentationBranch(latent_dim, num_clases)
+        # self.decoder = BasicDecoder(latent_dim, num_clases)
+        self.decoder = ImageSegmentationBranch(out_backbone, num_clases, latent_dim=latent_dim)
 
     def encode(self, input: Tensor) -> "list[Tensor]":
         result = self.encoder(input)
@@ -199,13 +221,11 @@ class VAEBackbone(BaseVAE):
         log_var = self.fc_var(result)
 
         return [mu, log_var]
-    
+
     def decode(self, z: Tensor) -> Tensor:
-        # z = self.pre_decoder(z)
-        # z = z.view(-1, 256, 3, 3)
         result = self.decoder(z)
         return result
-    
+
     def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
@@ -248,7 +268,7 @@ class VAEBackbone(BaseVAE):
 
     def generate(self, x: Tensor, **kwargs) -> Tensor:
         return self.forward(x)[0]
-        
+
 
 if __name__ == "__main__":
     import sys
